588aff2cd6830263d2e314459f21c8abcd2f97ba
MohamedRouias <146641169+MohamedRouias@users.noreply.github.com>
2025-04-01 10:14:15 +0000

    Excuse generator with a function finished

--- src/app.js
+++ src/app.js
@@ -5,7 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+
+
+function getRandomItem(array){
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  let randomWho = getRandomItem(who)
+  let randomAction = getRandomItem(action)
+  let randomWhat = getRandomItem(what)
+  let randomWhen = getRandomItem(when)
+  let excuse = document.getElementById("excuse")
+  excuse.innerText = randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen
 };
